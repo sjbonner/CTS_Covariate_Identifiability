9083726fe63b26944ae190c4f34737c51c9e3054
Simon Bonner <sbonner6@uwo.ca>
2020-06-04 09:54:01 -0400

    Switched to using gcc 7.3.0. Nimble does not find functions with gcc 8.3.0.

--- Simulation/simulation_slurm.sh
+++ Simulation/simulation_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=7:00:00
 #SBATCH --mem=5G
 
-module load gcc/8.3.0
+module load gcc/7.3.0
 module load r
 
 Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID &> Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
