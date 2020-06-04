56145ec61e6859e7db59fe8782253be59b2e9e32
Simon Bonner <sbonner6@uwo.ca>
2020-06-03 23:16:07 -0400

    Added module load gcc to allow use of R 4.0.0.

--- Simulation/simulation_slurm.sh
+++ Simulation/simulation_slurm.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --array=1-240
-#SBATCH --time=2:00:00
+#SBATCH --array=1-5
+#SBATCH --time=7:00:00
 #SBATCH --mem=5G
 
+module load gcc/8.3.0
 module load r
 
 Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID &> Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
