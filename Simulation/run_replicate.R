3c1d141b3047a7547dfea28c0cf658be4744b6b4
Simon Bonner <sbonner6@uwo.ca>
2020-06-08 16:47:54 -0400

    Fixed error in reading command line arguments.

--- Simulation/run_replicate.R
+++ Simulation/run_replicate.R
@@ -6,10 +6,10 @@ library(tidyverse)
 library(MASS)
 
 ## Load package containing common files
-library(CCI2020)
+## library(CCI2020)
 
-## library(devtools)
-## devtools::load_all("../CCI2020")
+library(devtools)
+devtools::load_all("../CCI2020")
 
 ## Source simulation specific code
 source("R/survival.R")
@@ -17,9 +17,12 @@ source("R/generate_data.R")
 source("R/simulation_functions.R")
 
 ## Read command line arguments
-args <- (trailingOnly = TRUE)
-k <- [1]
+args <- commandArgs(trailingOnly = TRUE)
+k <- args[1]
 pars_mat_name <- args[2]
+
+cat(k)
+cat(pars_mat_name)
 
 ## Load parameter matrix
 pars_mat <- read_csv(pars_mat_name)
