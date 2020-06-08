93002743b5c5225c8779c62284c59263b8364e6e
Simon Bonner <sbonner6@uwo.ca>
2020-06-08 16:40:43 -0400

    Corrected error in error and output files. Added parameter matrix file as an input argument to R.

--- Simulation/simulation_slurm_1.sh
+++ Simulation/simulation_slurm_1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --array=21-50,71-100,121-150,171-200,221-250,271-300,321-350,371-400,421-450
+#SBATCH --array=1-450
 #SBATCH --time=4:00:00
 #SBATCH --mem=5G
-#SBATCH --error=Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
-#SBATCH --output=Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
+#SBATCH --error=Logs/rep_%A_%a.Rout
+#SBATCH --output=Logs/rep_%A_%a.Rout
 
 
 module load gcc/7.3.0
 module load r
 
-Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID &> Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
+Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID parameter_matrix.csv &> Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
