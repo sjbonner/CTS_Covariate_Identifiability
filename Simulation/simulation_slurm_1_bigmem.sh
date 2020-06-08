b30cf1890046fbf6b39496a55a25faafadc34207
Simon Bonner <sbonner6@uwo.ca>
2020-06-08 16:39:25 -0400

    Corrected error in names of error and output files.

--- Simulation/simulation_slurm_1_bigmem.sh
+++ Simulation/simulation_slurm_1_bigmem.sh
@@ -2,11 +2,11 @@
 #SBATCH --array=451-600
 #SBATCH --time=10:00:00
 #SBATCH --mem=15G
-#SBATCH --error=Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
-#SBATCH --output=Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
+#SBATCH --error=Logs/rep_%A_%a.log
+#SBATCH --output=Logs/rep_%A_%a.log
 
 
 module load gcc/7.3.0
 module load r
 
-Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID &> Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
+Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID parameter_matrix.csv &> Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
