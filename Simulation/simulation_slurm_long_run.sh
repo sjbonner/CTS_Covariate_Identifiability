37d750a5666a26a8b1f66a354990acc6e920ccf2
Simon Bonner <sbonner6@uwo.ca>
2021-08-15 23:22:31 -0400

    Created single script to handle all runs of long run simulation.

--- Simulation/simulation_slurm_long_run.sh
+++ Simulation/simulation_slurm_long_run.sh
@@ -5,7 +5,6 @@
 #SBATCH --error=Logs/rep_%A_%a.log
 #SBATCH --output=Logs/rep_%A_%a.log
 
-module load gcc/7.3.0
 module load r
 
 Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID parameter_matrix_2.csv &> Logs/long_run_${SLURM_ARRAY_TASK_ID}.Rout
