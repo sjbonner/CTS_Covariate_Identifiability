73f80e7de505f81394830fb4588f2164aa0770b5
Simon Bonner <sbonner6@uwo.ca>
2021-08-15 23:23:00 -0400

    Created new script to handle all runs of the main simulation.

--- Simulation/simulation_slurm_main.sh
+++ Simulation/simulation_slurm_main.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --array=1-450
-#SBATCH --time=4:00:00
-#SBATCH --mem=5G
+#SBATCH --array=1-600
+#SBATCH --time=10:00:00
+#SBATCH --mem=15G
 #SBATCH --error=Logs/rep_%A_%a.Rout
 #SBATCH --output=Logs/rep_%A_%a.Rout
 
-
-module load gcc/7.3.0
 module load r
 
 Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID parameter_matrix.csv &> Logs/rep_${SLURM_ARRAY_TASK_ID}.Rout
