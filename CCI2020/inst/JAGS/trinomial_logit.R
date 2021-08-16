d7c234286604b8734e38949ce3a9895dd17ea981
Simon Bonner <sbonner6@uwo.ca>
2021-08-15 22:32:40 -0400

    Allowed user specified hyperparameters.

--- CCI2020/inst/JAGS/trinomial_logit.R
+++ CCI2020/inst/JAGS/trinomial_logit.R
@@ -1,35 +1,36 @@
 model{
 
-    ##### Likelihood #####
-
-    for(i in 1:nrelease){ ## i -- releases
-      ## Survival probability
-      logit(phi_tmp[i]) <- beta.phi[1] + beta.phi[2] * z[i]
-      phi[i] <- lower + (upper - lower) *  phi_tmp[i]
-      
-      ## Trinomial probabilities
-      Probs[i,2] <- phi[i] * p # Recapture
-      Probs[i,3] <- (1 - phi[i]) * lambda # Recovery
-      Probs[i,1] <- 1 - Probs[i,2] - Probs[i,3] # Neither
-
-      ## Event
-      outcome[i] ~ dcat(Probs[i,1:3])
-    }
-
-    ##### Priors #####
+  ##### Likelihood #####
+  
+  for(i in 1:nrelease){ ## i -- releases
     ## Survival probability
-    beta.phi[1] ~ dnorm(0,.01)
-    beta.phi[2] ~ dnorm(0,.01)
+    logit(phi_tmp[i]) <- beta.phi[1] + beta.phi[2] * z[i]
+    phi[i] <- lower + (upper - lower) *  phi_tmp[i]
+    
+    ## Trinomial probabilities
+    Probs[i,2] <- phi[i] * p # Recapture
+    Probs[i,3] <- (1 - phi[i]) * lambda # Recovery
+    Probs[i,1] <- 1 - Probs[i,2] - Probs[i,3] # Neither
+    
+    ## Event
+    outcome[i] ~ dcat(Probs[i,1:3])
+  }
+  
+  ##### Priors #####
+  ## Survival probability
+  for(k in 1:2){
+    beta.phi[k] ~ dnorm(beta.phi.hyper[k,1], beta.phi.hyper[k,2])
+  }
 
   ## Scale parameter
   lower <- 0
   upper <- 1
   
   ## Capture probability
-  p ~ dunif(0,1)
+  p ~ dunif(p.hyper[1],p.hyper[2])
 
   ## Recovery probability
-  lambda ~ dunif(0,1)
+  lambda ~ dunif(lambda.hyper[1],lambda.hyper[2])
 }
 
     
