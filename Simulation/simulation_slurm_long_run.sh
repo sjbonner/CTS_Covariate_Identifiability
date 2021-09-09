148f08c86e298c7bfb4bea315ba4275a7bbe4c68
Simon Bonner <sbonner6@uwo.ca>
2021-09-09 14:16:32 -0400

    Removed irrelevant slurm scripts.

--- Simulation/simulation_slurm_long_run.sh
+++ Simulation/simulation_slurm_long_run.sh
@@ -2,9 +2,9 @@
 #SBATCH --array=10-12
 #SBATCH --time=24:00:00
 #SBATCH --mem=30G
-#SBATCH --error=Logs/rep_%A_%a.log
-#SBATCH --output=Logs/rep_%A_%a.log
+#SBATCH --error=Logs/rep_%A_%a_long.log
+#SBATCH --output=Logs/rep_%A_%a_long.log
 
 module load r
 
-Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID parameter_matrix_2.csv &> Logs/long_run_${SLURM_ARRAY_TASK_ID}.Rout
+Rscript --vanilla run_replicate.R $SLURM_ARRAY_TASK_ID parameter_matrix_long.csv &> Logs/long_run_${SLURM_ARRAY_TASK_ID}.Rout
